82ff8b0790151bee708679eb2028e9f9a95d7c55
Keveny Filipe <kevenyfilipe@gmail.com>
2022-02-01 22:15:53 -0300

    Conexao com a API

---
+++ src/routes.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import HomeScreen from './screens/home'
+import RegisterScreen from './screens/auth/register'
+import LoginScreen from './screens/auth/login'
+import NotesScreen from './screens/notes/index'
+import UsersEditScreen from './screens/users/edit'
+
+const Routes = () => {
+    return (
+        <BrowserRouter>
+            <Switch>
+                <Route exact path='/' component={HomeScreen} />
+                <Route exact path='/register' component={RegisterScreen} />
+                <Route exact path='/login' component={LoginScreen} />
+                <Route exact path='/notes' component={NotesScreen} />
+                <Route exact path='/users/edit' component={UsersEditScreen} />
+            </Switch>
+        </BrowserRouter>
+    )
+}
+
+export default Routes
